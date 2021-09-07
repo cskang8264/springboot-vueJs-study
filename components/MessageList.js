58106a87864e66c17c94bd61b5de3f9ed3dd0119
gangdaewon <cskang8264@naver.com>
2021-09-07 15:59:36 +0900

    feat : components study

--- components/MessageList.js
+++ components/MessageList.js
@@ -4,20 +4,22 @@ export default {
     template: 
       `<ul>
         <message-list-item v-for="item in items" :item="item"
-        @delete="deleteMessage(item)"></message-list-item>   
+        :key="item.id" @delete="deleteMessage(item)"></message-list-item>   
       </ul>`,
+      // 상위 객체로 부터 넘겨받는 데이터
     props: {
       items: {
         type: Array,
         required: true
       }
     },
+    components: {
+        //자식 컴포넌트
+      MessageListItem
+      },
     methods: {
       deleteMessage (message) {
-        this.$emit('delete-each-message', message)
-      },
-    components: {
-      MessageListItem
-    }
+        this.$emit('delete', message)
+      }
     }
   }
