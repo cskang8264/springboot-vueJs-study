0fa5979de7f13326cb2d4fe16e24e81c1132a6ff
gangdaewon <cskang8264@naver.com>
2021-09-07 17:37:29 +0900

    feat : filter apply for massage date time

---
+++ filters/datetime.filter.js
@@ -0,0 +1,21 @@
+const formatter = new Intl.DateTimeFormat('en-US', {
+year: 'numeric', month: 'long', week: 'long', day: 'numeric',
+hour: 'numeric', minute: 'numeric', second: 'numeric'
+    });
+
+// 패턴 적용 전
+// Vue.filter('datetime', function(value) {
+//     if (!value) {
+//         return '';
+//     }
+//     return formatter.format(value);
+// })
+
+// 패턴 적용후
+// 아래와 같이 몇줄의 코드만으로 필터의 패턴 적용이 가능한 이유는 data-fns라이브러리를 사용하기 때문
+Vue.filter('datetime', function(value, pattern) {
+    if (!value) {
+        return '';
+    }
+    return formatter.format(value);
+})
