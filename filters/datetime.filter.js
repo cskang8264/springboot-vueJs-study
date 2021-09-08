5751d276f7406d3cc4e07b506eb7974ad6dd0ff8
gangdaewon <cskang8264@naver.com>
2021-09-08 10:46:28 +0900

    feat : mixin 활용한 라이프사이클 훅 단계별 콘솔찍어보기

--- filters/datetime.filter.js
+++ filters/datetime.filter.js
@@ -11,6 +11,9 @@ hour: 'numeric', minute: 'numeric', second: 'numeric'
 //     return formatter.format(value);
 // })
 
+
+
+
 // 패턴 적용후
 // 아래와 같이 몇줄의 코드만으로 필터의 패턴 적용이 가능한 이유는 data-fns라이브러리를 사용하기 때문
 Vue.filter('datetime', function(value, pattern) {
---
+++ mixins/lifecycle-logger.mixin.js
@@ -0,0 +1,15 @@
+// 각 라이프사이클 훅 함수에서 인스턴스 이름과 단계 간단히 출력
+export default {
+    created () {
+        console.log(`${this.$options.name} created`);
+    },
+    beforeMount() {
+        console.log(`${this.$options.name} beforeMount`);
+    },
+    mounted () {
+        console.log(`${this.$options.name} mounted`);
+    },
+    destroyed () {
+        console.log(`${this.$options.name} destroyed`);
+    }
+};
